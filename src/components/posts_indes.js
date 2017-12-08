a2860a4bd9713e92f42ceb9be16e62de935f988c
June Epstein <epstein.june@gmail.com>
2017-12-08 11:36:18 -0500

    adding redux-form

--- src/components/posts_indes.js
+++ src/components/posts_indes.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPosts(); 
   }
 
   renderPosts() {
@@ -21,6 +22,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className='text-xs-right'>
+          <Link className='btn btn-primary' to='/posts/new'>
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className='list-group'>
           {this.renderPosts()}
